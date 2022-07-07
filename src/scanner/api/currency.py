a245e3737392e390d730ceaee462d590ffa0c9f5
vladislavdedov <dedov_vlad@mail.ru>
2022-07-07 20:53:14 +0300

    1. Fix schema method's GET /actual 2. Add function tests

--- src/scanner/api/currency.py
+++ src/scanner/api/currency.py
@@ -6,6 +6,6 @@ from src.scanner.services.responce_currency import get_currency
 router = APIRouter(prefix="/currency")
 
 
-@router.get("/actual/", response_model=Currency, response_model_exclude_unset=True)
+@router.get("/actual/", response_model_exclude_unset=True)
 def get_actual_currency(currency_code: CurrencyCode = None):
     return get_currency(currency_code)
--- src/scanner/schemas/currency_schema.py
+++ src/scanner/schemas/currency_schema.py
@@ -1,11 +1,12 @@
 from enum import Enum
+from typing import Optional
 
 from pydantic import BaseModel
 
 
 class CurrencyCode(str, Enum):
-    USD = "USD"
-    EUR = "EUR"
+    USD: Optional = "USD"
+    EUR: Optional = "EUR"
 
 
 class Currency(BaseModel):
