219cec7d795823569ad777e8b4276a71b6f5ec16
vladislavdedov <dedov_vlad@mail.ru>
2022-07-07 23:44:59 +0300

    Add run server in app.py

--- src/scanner/app.py
+++ src/scanner/app.py
@@ -1,6 +1,16 @@
+import uvicorn
 from fastapi import FastAPI
 
-from .api import router
+from src.scanner.api import router
+from src.scanner.settings import settings
 
 app = FastAPI()
 app.include_router(router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app=app,
+        host=settings.server_host,
+        port=settings.server_port,
+    )
