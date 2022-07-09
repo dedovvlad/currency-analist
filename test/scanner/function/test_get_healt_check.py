f12fad3b89c5e6b8d6a9aa76ea91e14d73112a5a
vladislavdedov <dedov_vlad@mail.ru>
2022-07-09 10:31:41 +0300

    Add tests` names

--- test/scanner/function/test_get_healt_check.py
+++ test/scanner/function/test_get_healt_check.py
@@ -1,7 +1,5 @@
 def test_get_healt_check(client):
+    """Тест для проверки запроса хелсчека."""
     response = client.get("/health_check")
     assert response.status_code == 200
     assert response.json() == {"status": "OK"}
-
-
-# TODO: Дописать названия тестов
